49576e1aea6890ec350102cb9bddc77564111987
Narbonnais <narbonnais@users.noreply.github.com>
2022-11-06 11:31:33 +0000

    Added cw721

---
+++ contracts/cw721/examples/schema.rs
@@ -0,0 +1,16 @@
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use cosmwasm_std::Empty;
+use cw721_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use std::env::current_dir;
+use std::fs::create_dir_all;
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg<Empty, Empty>), &out_dir);
+    export_schema(&schema_for!(QueryMsg<Empty>), &out_dir);
+}
